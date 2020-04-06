7228a430c7270c46bff4ba84d66b52bc9950d862
Aidensaid <ai.loughlin@gmail.com>
2020-04-07 08:59:27 +1000

    Created inline validation for password field

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ email.addEventListener('input', function (event) {
     emailError.innerHTML = '';
     emailError.className = 'error'
   } else {
-    showError()
+    showEmailError()
   }
 });
 
 form.addEventListener('submit', function (event) {
   if (!email.validity.valid) {
-    showError();
+    showEmailError();
     event.preventDefault()
   }
 });
 
-function showError() {
+function showEmailError() {
   if (email.validity.valueMissing) {
     emailError.textContent = 'You need to enter an e-mail address.'
   } else if (email.validity.typeMismatch) {
@@ -38,19 +38,28 @@ const countryCode = document.getElementById('country-code')
 const countryCodeError = document.querySelector('#country-code +span.error')
 
 countryCode.addEventListener('input', function (event) {
+  l(countryCode.validity.valid)
   if (countryCode.validity.valid) {
     countryCodeError.innerHTMl = '';
     countryCodeError.className = 'error'
   } else {
     l('error')
-    showError()
+    showCountryError()
   }
 });
 
-function showError() {
+form.addEventListener('submit', function (event) {
+  if (!countryCode.validity.valid) {
+    showEmailError();
+    event.preventDefault()
+  }
+});
+
+function showCountryError() {
+
   if (countryCode.validity.valueMissing) {
     countryCodeError.textContent = 'You need to enter a country code'
-  } else if (countryCode.validity.typeMismatch) {
+  } else if (countryCode.validity.patternMismatch) {
     countryCodeError.textContent = 'Entered value must be a number'
   } else if (countryCode.validity.tooLong) {
     countryCodeError.textContent = `Country code should be a maximum of ${countryCode.maxLength} characters; you entered ${countryCode.value.length}; `
@@ -58,7 +67,28 @@ function showError() {
   emailError.className = 'error active'
 }
 
+const password = document.getElementById('password')
+const passwordError = document.querySelector('#password +span.error')
 
+password.addEventListener('input', function (event) {
+  if (password.validity.valid) {
+    passwordError.innerHTML = ''
+    passwordError.className = 'error'
+  } else {
+    showPasswordError()
+  }
+})
 
+form.addEventListener('submit', function (event) {
+  if (!password.validity.valid) {
+    showPasswordError();
+    event.preventDefault()
+  }
+})
 
+function showPasswordError() {
+  if (password.validity.tooShort) {
+    passwordError.textContent = `Your password must be a minimum of ${password.minLength} characters; you entered ${password.value.length}.`
+  }
+}
 
